9776d5a54edc6f3f6d0e062c328652063f140d39
IndianBoy42 <rohanmed123@gmail.com>
2020-11-22 17:39:01 +0800

    try these model export

--- run_lp_detect_recog.py
+++ run_lp_detect_recog.py
@@ -11,8 +11,11 @@ weights_file_detect = 'weights/yolov5s_detect.pt'
 
 iou_recog = 0.3
 conf_recog = 0.5
-img_size_recog = 416
+img_size_recog = 224
 weights_file_recog = 'weights/yolov5s_recog.pt'
+
+# os.system(f"python models/export.py --weights {weights_file_detect} --img-size  {img_size_detect} --batch-size 1")
+# os.system(f"python models/export.py --weights {weights_file_recog} --img-size  {img_size_recog} --batch-size 1")
 
 # Detect and recog
 os.system(f"python lp_detect_recog.py --img-size_detect  {img_size_detect} --img-size_recog  {img_size_recog}"
